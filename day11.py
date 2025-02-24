7575bcf30e88a57b653a0e0e7e7e7ad120eaf48b
Youngmin98kim <sodayoung73@gmail.com>
2025-02-24 11:38:54 +0900

    v0.2 day11 pandas

--- day11.py
+++ day11.py
@@ -2,3 +2,15 @@ import numpy as np
 v = np.array([1,3.9,-9,2])
 print(v,v.ndim)
 #numpy의 기능? 내부 원소를 같은 type으로 만들도록 설계
+
+import pandas as pd
+
+df = pd.DataFrame ({"a":[4,5,6],"b":[7,8,9],"c":[10,11,12]}, index = [1,2,3]
+ )
+
+print(df)
+#key : column 명칭
+
+#2차원 리스트를 사용하는 방법
+df = pd.DataFrame([[4,7,10],[5,8,11],[6,9,12]], index=[1,2,3],columns = ['a','b','c'])
+print(df)
